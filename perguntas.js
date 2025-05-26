1baaae278360abbe819483533ddbd27f690da75d
anabibia11022009 <anabeatrizdeandradecanga@gmail.com>
2025-05-26 19:20:23 -0300

    Rhaissa maldita

---
+++ perguntas.js
@@ -0,0 +1,79 @@
+criaCartao(
+    'Imapactos das Mudanças climáticas',
+    'Quais são as consequências que as mudanças climáticas trazem?',
+    'As mudanças climáticas estão causando preocupações significativas para a vida polar, pois o derretimento do gelo marinho e o aumento da temperatura afetam a sobrevivência de muitas espécies'
+)
+
+criaCartao(
+    'Adaptados a um Mundo de Gelo e Neve',
+    'Quais animais habitam no mundo da neve Ártico?',
+    'Ursos Polares, Raposas Ártica, Baleias, Morsas, Caribus, Corujas-das-Neves, Lobo Ártico.'
+)
+
+criaCartao(
+    'Adaptados a um Mundo de Gelo e Neve',
+    'Quais animais habitam no mundo da neve Antártico?',
+    'Pinguns, Baleias, Aves, Foca, Peixes, Crustáceos.'
+)
+
+criaCartao(
+    'Flora e Fauna',
+    'Como é a flora e a fauna polar?',
+    'A fauna polar inclui animais como ursos polares, focas, pinguins, raposas do Ártico e renas, além de diversas espécies de aves marinhas. A flora é limitada e geralmente composta por musgos, líquenes e algumas plantas adaptadas ao clima frio'
+)
+
+criaCartao(
+    'Vegetação',
+    'Vocês sabe como é a vegetação do território polar e como ela funciona?',
+    'A Vegetação do territóriopolar refere-se às regiões do planeta Terra localizadas entre os círculos polares (Círculo Polar Ártico a norte e Círculo Polar Antártico a sul) e os polos Norte e Sul. Estas áreas são caracterizadas por climas frios, com temperaturas baixas e longos invernos, e são marcadas por paisagens de gelo, neve e tundra.'
+)
+
+criaCartao(
+   ' Animais Polares' ,
+    'Você sabe o que são animais polares, e como vivem?',
+    'Animais Polares são os animais que vivem nos polos polares da Terra, como pinguim, focas, ursos polares e tantos outros animais, seu estilo de vida é totalmente diferenciado dos animais do calor, consequentemente, é mais curiosos e diverdito.'
+)
+
+criaCartao(
+    'Noites e Dias Polares',
+    'Você sabe como funciona as noites e os dias polares?',
+    'As regiões polares experimentam períodos de noite polar (longos períodos de escuridão) e dia polar (longos períodos de luz solar) durante os meses de inverno e verão, respectivamente. A duração desses períodos depende da latitude.'
+)
+
+criaCartao(
+    'Ambiente Polar',
+    ' Como são os ambientes polares?',
+    'Os mbientes polares são rigorosos, como o Ártico e a Antártida, onde as temperaturas são extremamente baixas, chegando a -60°C em algumas regiões. O gelo e a neve são características marcantes desses locais.'
+)
+
+criaCartao(
+    'Tundra',
+    'O que é Tundra?',
+    'É o bioma dominante nas regiões polares, caracterizado pela vegetação rasteira e por plantas adaptadas às condições climáticas rigorosas. '
+)
+
+
+criaCartao(
+    'Ausência De Árvores',
+    ' Por que há uma ausência de árvores no território polar?',
+    'As árvores não conseguem sobreviver em grande parte do Ártico e da Antártida devido às baixas temperaturas, ao permafrost (solo congelado) e aos ventos fortes.'
+)
+
+criaCartao(
+    'Biodiversidade',
+    ' Será que existe Biodiversidade no mundo do gelo?',
+    'Apesar das condições adversas, a vida polar é surpreendentemente diversificada, com plantas e animais que conseguem sobreviver e se reproduzir em ambientes que seriam considerados hostis para a maioria das espécies'
+)
+
+criaCartao(
+    'Pinguins',
+    ' Você já ouviu falar das fofurinhas chamadas Pinguins?',
+    'Os pinguins são aves marinhas que possuem uma incrível capacidade de nadar em águas frias e praticam a reprodução em ambientes hostis, com filhotes que se desenvolvem em condições extremas.'
+)
+
+criaCartao(
+    'Pingo',
+    ' Você já Ouviu falar do Pingo?',
+    'Pingo é um desenho animado infantil, que conta a historia de Pingo, um pinguim, e sua familia. Veja um trecho e se encante por esse belo pinguim!'
+)
+
